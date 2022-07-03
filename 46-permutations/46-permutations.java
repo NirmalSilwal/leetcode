6aa8639b48e0ef90387a7421acd3ef4029b5ef93
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-07-03 10:11:07 -0500

    Time: 2 ms (74.57%), Space: 45.3 MB (13.88%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,4 +1,7 @@
 class Solution {
+    
+    // 1st approach
+    /*
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         
@@ -29,7 +32,47 @@ class Solution {
                 templist.remove(templist.size() - 1);
                 map[i] = false;
             }
+        }    
+    }
+    */
+    // 2nd approach -  without extra space
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        findPermutations2(nums, 0, result);
+        
+        return result;
+    }
+    
+    private void findPermutations2(int[] nums, int index, List<List<Integer>> result) {
+       
+        if (index == nums.length) {
+            // permutation is formed, as the result is itself in original array nums, we copy it and store in result
+            List<Integer> templist = new ArrayList<>();
+            
+            for (int i = 0; i < nums.length; i++) {
+                templist.add(nums[i]);
+            }
+            // now storing in result array
+            result.add(new ArrayList<>(templist));
+            return;
         }
         
+        // scanning right indexes from current index
+        for (int i = index; i < nums.length; i++) {
+            
+            swap(i, index, nums); // swap with yourself and swap with all elements in right indexes from you
+            
+            findPermutations2(nums, index + 1, result);
+            
+            swap(i, index, nums); // backtrack and swap previously swapped elements
+        }
     }
+    
+    private void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
 }
