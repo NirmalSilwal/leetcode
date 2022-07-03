17708561d94e6b6162ff6e30890f5d31f3862277
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-07-03 07:49:33 -0500

    Time: 2 ms (74.57%), Space: 44.5 MB (61.29%) - LeetHub

---
+++ 46-permutations/46-permutations.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        boolean[] map = new boolean[nums.length];
+        
+        findPermutations(nums, 0, new ArrayList<Integer>(), map, result);
+        
+        return result;
+    }
+    
+    private void findPermutations(int[] nums, int index, List<Integer> templist, boolean[] map, List<List<Integer>> result) {
+        // when permutation if formed
+        if (templist.size() == nums.length) {
+            result.add(new ArrayList<>(templist));
+            return;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            
+            if (!map[i]) { // if current index is not taken
+                map[i] = true;
+                
+                templist.add(nums[i]);
+                
+                findPermutations(nums, index + 1, templist, map, result);
+                
+                // backtrack
+                templist.remove(templist.size() - 1);
+                map[i] = false;
+            }
+        }
+        
+    }
+}
