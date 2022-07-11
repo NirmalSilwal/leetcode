08288c402686248c4f6a34fe41520a48b0c40253
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-07-10 23:49:21 -0500

    Time: 0 ms (100.00%), Space: 42.1 MB (73.72%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,6 +1,7 @@
 class Solution {
     
     // two pass solution
+    /*
     public void sortColors(int[] nums) {
         int[] frequency = new int[3]; // each index will hold count of their indices value in array
         
@@ -32,5 +33,29 @@ class Solution {
             nums[i] = 2;
             currIndex++;
         }
+    }
+    */
+    
+    // one pass approach - dutch national flag algorithm
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+        
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 2) {
+                swap(nums, high, mid);
+                high--;
+            }
+            else mid++;
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
