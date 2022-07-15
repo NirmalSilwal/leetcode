119efd0137beb6eb25f27760483657b3e8dd674b
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-07-15 18:48:34 -0500

    Time: 13 ms (86.32%), Space: 192.3 MB (55.94%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,4 +1,6 @@
 class Solution {
+    // approach 1: only backtracking
+    /*
     public List<List<String>> partition(String s) {
         List<List<String>> result = new ArrayList<>();
 
@@ -34,4 +36,40 @@ class Solution {
         }
         return true;
     }
+    
+    */
+    
+    // approach 2: dp + backtracking
+    
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        
+        findPartitions(0, s, new ArrayList<String>(), result, dp);
+        
+        return result;
+    }
+    
+      private void findPartitions(int start, String str, List<String> path, List<List<String>> result, boolean[][] dp){
+          if (start == str.length()) {
+              result.add(new ArrayList<>(path));
+              return;
+          }
+          
+          for (int end = start; end < str.length(); end++) {
+              
+              if (str.charAt(start) == str.charAt(end) && (end - start <= 2 || dp[start+1][end-1])) {
+                  
+                  dp[start][end] = true;
+                  
+                  path.add(str.substring(start, end + 1));
+                  
+                  findPartitions(end + 1, str, path, result, dp);
+                  
+                  path.remove(path.size() - 1);
+              }
+          }
+      }
+    
 }
