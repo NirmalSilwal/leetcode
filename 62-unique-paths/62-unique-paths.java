d119d62fb6c7999c13a316ff9625d26fe69d0411
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-07-16 13:34:36 -0500

    Time: 1 ms (53.06%), Space: 41.8 MB (5.86%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -35,10 +35,9 @@ class Solution {
         // backtrack and mark as unvisited
         visited[currRow][currCol] = false;
     }
-    */
     
     
-    // approach 2 optimization: DP with recursion
+    // approach 2 optimization: top down DP with recursion
     public int uniquePaths(int m, int n) {
         
         return uniquePathsTopDownDP(0, 0, m, n, new int[m][n]);
@@ -46,7 +45,7 @@ class Solution {
     
     private int uniquePathsTopDownDP(int currRow, int currCol, int m, int n, int[][] storage) {
         
-        if (currRow == m-1 && currCol == n-1) {
+        if (currRow == m - 1 && currCol == n - 1) {
             return 1;
         }
         
@@ -68,4 +67,33 @@ class Solution {
         
         return totalCounts;
     }
+    
+    */
+    
+    
+    // approach 3 optimization: bottom up DP, space/ recursive call stack optimization
+    
+    public int uniquePaths(int m, int n) {
+
+        // extra row and column- dummy row below last row and dummy column after last column 
+        int[][] storage = new int[m + 1][n + 1]; 
+        
+        // filling right to left and bottom to top in the storage array for our answer
+        
+        for (int row = m-1; row >= 0; row--) {
+            
+            for (int col = n-1; col >= 0; col--) {
+                
+                if (row == m-1 && col == n-1) { // our finish line
+                    storage[row][col] = 1;
+                } else {
+                    // adding answer from it's right and bottom cell value
+                    storage[row][col] = storage[row][col + 1] + storage[row + 1][col];
+                }
+            }
+        }
+        
+        return storage[0][0];
+    }
+   
 }
