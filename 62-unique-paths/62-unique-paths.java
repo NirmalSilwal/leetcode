d7acb1bcc4f98f903ade5a247f4d44b400513385
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-07-16 12:15:07 -0500

    Time: 1 ms (53.06%), Space: 41.3 MB (26.87%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,71 @@
+class Solution {
+    
+    // recursion solution - TLE
+    /*
+    int count = 0;
+    public int uniquePaths(int m, int n) {
+        
+        boolean[][] visited = new boolean[m][n];
+        
+        mazePaths(0, 0, m, n, visited);
+         
+        return count;
+    }
+    
+    private void mazePaths(int currRow, int currCol, int m, int n, boolean[][] visited) {
+        
+        if (currRow == m - 1 && currCol == n - 1) {
+            count++; // path found
+            return;
+        }
+        
+        // boundary checks
+        if (currRow == -1 || currCol == -1 || currRow == m || currCol == n || visited[currRow][currCol]) {
+            return;
+        }
+        
+        visited[currRow][currCol] = true; // mark current position as visited
+        
+        // move down
+        mazePaths(currRow + 1, currCol, m, n, visited);
+        
+        // move right
+        mazePaths(currRow, currCol + 1, m, n, visited);
+        
+        // backtrack and mark as unvisited
+        visited[currRow][currCol] = false;
+    }
+    */
+    
+    
+    // approach 2 optimization: DP with recursion
+    public int uniquePaths(int m, int n) {
+        
+        return uniquePathsTopDownDP(0, 0, m, n, new int[m+1][n+1]);
+    }
+    
+    private int uniquePathsTopDownDP(int currRow, int currCol, int m, int n, int[][] storage) {
+        
+        if (currRow == m-1 && currCol == n-1) {
+            return 1;
+        }
+        
+        if (currRow >= m || currCol >= n) {
+            return 0;
+        }
+        
+        if (storage[currRow][currCol] != 0) {
+            return storage[currRow][currCol];
+        }
+        
+        int moveDownCounts = uniquePathsTopDownDP(currRow + 1, currCol, m, n, storage);
+        
+        int moveRightCounts = uniquePathsTopDownDP(currRow, currCol + 1, m, n, storage);
+
+        int totalCounts = moveDownCounts + moveRightCounts;
+        
+        storage[currRow][currCol] = totalCounts;
+        
+        return totalCounts;
+    }
+}
