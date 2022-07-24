8c3f30821f0aa924feac62cc1c0c1e1a4395412a
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-07-24 15:24:59 -0500

    Time: 10 ms (64.73%), Space: 58.4 MB (9.70%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -1,5 +1,6 @@
 class Solution {
-    // brute force - O(mn) solution
+    // brute force - O(mn) time, O(1) space
+    /*
     public boolean searchMatrix(int[][] matrix, int target) {
         
         for (int i = 0; i < matrix.length; i++) { // rows
@@ -10,4 +11,32 @@ class Solution {
         }
         return false;
     }
+    */
+    
+    
+    // approach 2 - optimal O(n+m) time
+        public boolean searchMatrix(int[][] matrix, int target) {
+            
+            if (matrix == null || matrix.length  < 1) 
+                return false;
+            
+            // starting from top right corner
+            int row = 0; 
+            int col = matrix[0].length - 1;
+            
+            while (col >= 0 && row < matrix.length) {
+                
+                if (matrix[row][col] == target) {
+                    return true;
+                } else if (matrix[row][col] > target){
+                    col--;
+                } else if (matrix[row][col] < target) {
+                    row++;
+                }
+            }
+            
+            return false;
+        }
+
+
 }
