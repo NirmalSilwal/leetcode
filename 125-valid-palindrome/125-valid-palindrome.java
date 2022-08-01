83873e9aaa61c735828f7b861bf644d0672c6be1
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-08-01 15:13:17 -0500

    Time: 4 ms (89.85%), Space: 43.4 MB (77.89%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,4 +1,6 @@
 class Solution {
+
+    /*
     public boolean isPalindrome(String s) {
 
         s = s.toLowerCase().trim();
@@ -14,6 +16,31 @@ class Solution {
             low++;
             high--;
         }
+        return true;
+    }
+    */
+    public boolean isPalindrome(String s) {
+        if (s.length() == 1) return true;
+        
+        int low = 0, high = s.length() - 1;
+        
+        while (low < high) {
+            
+            while (low < high && !Character.isLetterOrDigit(s.charAt(low))) {
+                low++;
+            }
+            
+            while (low < high && !Character.isLetterOrDigit(s.charAt(high))){
+                high--;
+            }
+            
+            if (Character.toLowerCase(s.charAt(low)) != Character.toLowerCase(s.charAt(high)))
+                return false;
+            
+            low++;
+            high--;
+        }
+        
         return true;
     }
 }
