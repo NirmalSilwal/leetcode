5fdc13794eb6d5491ec0c7abc315a7c68de26a9e
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-08-02 14:32:10 -0500

    Time: 5 ms (90.01%), Space: 42.3 MB (94.02%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -25,6 +25,7 @@ class Solution {
     */
     
     // 2nd approach -  using Collections reverse functionality
+    /*
     public String reverseWords(String s) {
         
         String[] words = s.trim().split(" +");
@@ -32,5 +33,38 @@ class Solution {
         Collections.reverse(Arrays.asList(words));
         
         return String.join(" ", words);
+    }
+    */
+    
+    // 3rd approach
+    public String reverseWords(String s) {
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < s.length(); i++) {
+            
+            // if space is found, keep moving pointer i
+            if (s.charAt(i) == ' ') continue;
+            
+            // now since we encounter some character rather than space, we track this as start index
+            int start = i;
+            
+            // now tracking where our word ends till next space is found, 
+            // as our start of word is tracked by start pointer, now end of word will be tracked by i
+            while (i < s.length() && s.charAt(i) != ' ') {
+                i++;
+            }
+            
+            // now word is found, so add in our result - but for 1st word result will be empty 
+            // remember - our word is in position from start to i position like we calculated above
+            if (result.length() == 0) {
+                result.insert(0, s.substring(start, i));
+            } else {
+                // initially add one space in front of result and then add new word
+                result.insert(0, " ");
+                result.insert(0, s.substring(start, i));
+            }
+        }
+        
+        return result.toString();
     }
 }
