1636ef1e682c62c63aa273897a289a911fe67ada
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-08-13 22:50:58 -0500

    Time: 7 ms (33.91%), Space: 48.5 MB (14.34%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.java
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.java
@@ -10,27 +10,29 @@
  */
 class Solution {
     // approach 1 - by reversing both list and reversing result list also
+    /*
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         
         ListNode rev1 = reverse(l1);
         ListNode rev2 = reverse(l2);
+        
         int carryover = 0;
         ListNode dummyHead = new ListNode(-1);
         ListNode dummy = dummyHead;
         
         while (rev1 != null || rev2 != null || carryover != 0) {
-            int currsum = 0;   
+            int currsum = 0;
+            
             if (rev1 != null) {
                 currsum += rev1.val;
-            }            
+            }
+            
             if (rev2 != null) {
                 currsum += rev2.val;
-            }            
-            currsum += carryover;
+            }
             
-            // new carryover for next addition, 
-            // since input range is 0 to 9, max sum will be 9 + 9 = 18, so carryover will be either 1 or 0
-            carryover = currsum > 9 ? 1 : 0; 
+            currsum += carryover;
+            carryover = currsum > 9 ? 1 : 0; // new carryover for next addition
             
             ListNode newNode = new ListNode(currsum % 10); // create new node
             
@@ -45,6 +47,7 @@ class Solution {
         
         return result;
     }
+    */
     
     // reverse linkedlist
     private ListNode reverse(ListNode head) {
@@ -63,5 +66,63 @@ class Solution {
         head = curr;
         
         return head;
+    }
+    
+    
+    // Follow up: without reversing input list
+    
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        
+        ListNode temp1 = l1, temp2 = l2;
+        
+        while (temp1 != null) {
+            stack1.push(temp1.val);
+            temp1 = temp1.next;
+        }
+        
+        while (temp2 != null) {
+            stack2.push(temp2.val);
+            temp2 = temp2.next;
+        }
+        
+        ListNode dummyHead = new ListNode(-1);
+        ListNode dummy = dummyHead;
+        int carryover = 0;
+        
+        while (!stack1.isEmpty() && !stack2.isEmpty()) {
+            
+            int currsum = stack1.pop() + stack2.pop() + carryover;
+            
+            dummy.next = new ListNode(currsum % 10);
+            dummy = dummy.next;
+            
+            carryover = currsum > 9 ? 1 : 0;
+        }
+        
+        // now see if one of the stack has left some elements
+        while (!stack1.isEmpty()) {
+            int currsum = stack1.pop() + carryover;
+            
+            dummy.next = new ListNode(currsum % 10);
+            dummy = dummy.next;
+            carryover = currsum > 9 ? 1 : 0;
+        }
+        
+        while (!stack2.isEmpty()) {
+            int currsum = stack2.pop() + carryover;
+            
+            dummy.next = new ListNode(currsum % 10);
+            dummy = dummy.next;
+            carryover = currsum > 9 ? 1 : 0;
+        }
+        
+        // if last carryover is left like in test case l1=[5] and l2=[5]
+        if (carryover == 1)
+            dummy.next = new ListNode(1);
+        
+        // now reversing result
+        return reverse(dummyHead.next);
     }
 }
