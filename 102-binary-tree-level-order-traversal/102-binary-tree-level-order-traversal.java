98321fe5e5eb1ea600ee4a7892fc4a0885ee7552
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-08-19 16:39:10 -0500

    Time: 1 ms (93.23%), Space: 43.1 MB (82.11%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -14,18 +14,19 @@
  * }
  */
 class Solution {
-    // 1st approach: bfs
+    // 1st approach: bfs, O(N) time, O(N) space
+    /*
     public List<List<Integer>> levelOrder(TreeNode root) {
         
         List<List<Integer>> traversal = new ArrayList<>();
-        
         if (root == null) { // edge case
             return traversal;
         }  
+        
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
               
-        while (!queue.isEmpty()) { 
+        while (!queue.isEmpty()) { // bfs
             
             int levelSize = queue.size();
             List<Integer> currentLevel = new ArrayList<>();
@@ -44,6 +45,28 @@ class Solution {
             }
             traversal.add(new ArrayList<>(currentLevel));
         }
+        
         return traversal;
+    }
+    */
+    
+    // 2nd approach - recursive
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> traversal = new ArrayList<>();
+        dfs(root, 1, traversal);
+        return traversal;
+    }
+    
+    private void dfs(TreeNode root, int level, List<List<Integer>> traversal) {
+        
+        if (root == null) return;
+        
+        if (traversal.size() < level) {
+            traversal.add(new ArrayList<>());
+        }
+        traversal.get(level-1).add(root.val);
+        
+        dfs(root.left, level + 1, traversal);
+        dfs(root.right, level + 1, traversal);
     }
 }
