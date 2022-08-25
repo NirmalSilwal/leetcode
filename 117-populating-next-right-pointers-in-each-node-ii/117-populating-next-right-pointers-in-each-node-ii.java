4f6cd9cb3b2a10148646636e7aa1d24d6450f3be
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-08-25 01:18:41 -0500

    Time: 0 ms (100.00%), Space: 43.7 MB (83.63%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -22,7 +22,9 @@ class Node {
 */
 
 class Solution {
-    // extra space with bfs, level order traversal
+    /*
+    // 1st approach - extra space with bfs, level order traversal
+    // O(N) time, O(N) space
     public Node connect(Node root) {
         
         if (root == null) return root;
@@ -50,6 +52,37 @@ class Solution {
             }
         }
         
+        return root;
+    }
+    */
+    
+    // follow up - O(1) space
+    public Node connect(Node root) { 
+        if (root == null) 
+            return root;   
+
+        Node head = root;
+        
+        // build linked list
+        while (head != null) {
+            
+            Node dummy = new Node();
+            Node temp = dummy;
+            
+            while (head != null) {
+                if (head.left != null) {
+                    temp.next = head.left;
+                    temp = temp.next;
+                }
+                if (head.right != null) {
+                    temp.next = head.right;
+                    temp = temp.next;
+                }
+                head = head.next;
+            }
+            // start from next level
+            head = dummy.next;
+        }
         return root;
     }
 }
