e70a0187cac7080b8ea97a009d4fb9975a798bc2
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-08-30 20:02:28 -0500

    Time: 0 ms (100.00%), Space: 40.6 MB (65.31%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -1,11 +1,11 @@
 class Solution {
             
+    // 1st approach
+    /*
     int[] dp = new int[20];
-    
     public int numTrees(int n) {
         
-        if (n <= 1) 
-            return 1;
+        if (n <= 1) return 1;
         
         if (dp[n] > 0) 
             return dp[n];
@@ -16,4 +16,34 @@ class Solution {
         
         return dp[n];
     }
+    */
+    
+    // 2nd approach
+    
+    /*  f(4) = f(0)*f(3) + f(1)*f(2) + f(2)*f(1) + f(3)*f(0)
+        f(x) means total BST formed with x number of nodes
+        for n = 4, try making root node as 1, 2, 3 and 4 each and form left and right subtree individually
+        idea of Catlan number
+    */
+    
+    public int numTrees(int n) {
+        
+        int[] dp = new int[n+1];
+    
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            
+            int left = 0;
+            int right = i - 1;
+            
+            while (left <= i-1) {
+                dp[i] += dp[left] * dp[right];
+                left++;
+                right--;
+            }
+        }
+        return dp[n];
+    }    
 }
