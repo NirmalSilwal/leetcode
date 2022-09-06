6d5134c4a4b4b3be3e8567d3f8ead25cf05a8a71
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-09-06 01:13:23 -0500

    Time: 10 ms (18.84%), Space: 43.2 MB (21.45%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,20 +1,19 @@
 class Solution {
+    // 1st approach
+    /*
     public List<String> letterCombinations(String digits) {
                 
         List<String> result = new ArrayList<>();
 
-        if (digits == null || digits.length() == 0) 
-            return result;
+        if (digits == null || digits.length() == 0) return result;
         
         String[] charmap = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         
-        findCombinations("", 0, digits, charmap, result);
-        
+        letterCombinationsRecursive("", 0, digits, charmap, result);
         return result;
     }
     
-    private void findCombinations(String current, int index, String digits, String[] charmap, List<String> result) {
-        
+    private void letterCombinationsRecursive(String current, int index, String digits, String[] charmap, List<String> result) {
         if (index == digits.length()) {
             result.add(current);
             return;
@@ -23,7 +22,46 @@ class Solution {
         String letters = charmap[digits.charAt(index) - '0'];
         
         for (int i = 0; i < letters.length(); i++) {
-            findCombinations(current + letters.charAt(i), index + 1, digits, charmap, result);
+            letterCombinationsRecursive(current + letters.charAt(i), index + 1, digits, charmap, result);
         }
+    }
+    */
+    
+    // 2nd approach
+    public List<String> letterCombinations(String digits) {       
+                
+        if (digits == null || digits.length() == 0) 
+            return new ArrayList<String>();
+        
+        String[] charmap = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};            
+        
+        return helper(digits, charmap);        
+    }
+   
+    public ArrayList<String> helper(String digits, String[] charmap) {
+        
+        if (digits.length() == 0) {
+            ArrayList<String> baseResult = new ArrayList<>();
+            baseResult.add("");
+            return baseResult;
+        }
+        
+        char ch = digits.charAt(0);
+        String ros = digits.substring(1); // rest of string
+        
+        ArrayList<String> initialResult = helper(ros, charmap);
+        
+        ArrayList<String> combinedResult = new ArrayList<>();
+        String letters = charmap[ch - '0'];
+        
+        for (int i = 0; i < letters.length(); i++) {
+            char current = letters.charAt(i);
+            
+            for (String str : initialResult) {
+                combinedResult.add(current + str);
+            }
+        }
+        
+        return combinedResult;
     }
 }
