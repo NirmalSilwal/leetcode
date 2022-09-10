8d01650125233c21e78b938376ec3d6601d6818e
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-09-10 01:27:29 -0500

    Time: 2 ms (48.14%), Space: 42.6 MB (65.34%) - LeetHub

---
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // approach 1 idea - using bfs, in queue last element in each level will be right view
+    public List<Integer> rightSideView(TreeNode root) {
+        
+        List<Integer> view = new ArrayList<>();       
+        if (root == null) return view;       
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int qsize = queue.size(); // each level size
+            
+            for (int i = 0; i < qsize; i++) {
+                
+                TreeNode removed = queue.poll();
+                
+                if (i == qsize - 1)
+                    view.add(removed.val);
+               
+                if (removed.left != null) {
+                     queue.offer(removed.left);
+                }                    
+                if (removed.right != null) {
+                    queue.offer(removed.right);
+                }
+            }
+        }   
+        return view;
+    }
+    
+}
