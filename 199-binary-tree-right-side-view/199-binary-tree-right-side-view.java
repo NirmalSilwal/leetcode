e2619dbfa6be82c934e1cddc8c7f6aab5131431e
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-09-10 10:49:55 -0500

    Time: 1 ms (95.04%), Space: 42.8 MB (48.51%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -53,6 +53,7 @@ class Solution {
         if (root == null) return view; 
                 
         rightView(root, view, 0);
+        // leftView(root, view);
         
         return view;
     }
@@ -68,5 +69,31 @@ class Solution {
         rightView(root.right, view, currentLevel + 1);
         
         rightView(root.left, view, currentLevel + 1);
+    }
+    
+    // left view of binary tree
+    // idea: 1st node in each level in bfs / level order traversal would give left view
+    private void leftView(TreeNode root, List<Integer> view) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root); // add in queue
+        
+        while (!queue.isEmpty()) {
+            
+            int levelSize = queue.size();
+            
+            for (int i = 0; i < levelSize; i++) {
+
+                TreeNode removed = queue.poll();
+
+                if (i == 0)
+                    view.add(removed.val);
+                
+                if (removed.left != null) 
+                    queue.add(removed.left);
+                
+                if (removed.right != null)
+                    queue.add(removed.right);
+            }
+        }
     }
 }
