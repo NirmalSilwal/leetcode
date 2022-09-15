52bf4c7e9567cfbff2e52e5bb7b6022f8c718d76
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-09-14 23:34:42 -0500

    Time: 4 ms (34.09%), Space: 42.4 MB (23.95%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,38 +1,24 @@
 class Solution {
-    // idea- push in stack for open paranthesis and pop for closed ones
-    
     public boolean isValid(String s) {
+        if (s == null || s.length() == 0)
+            return true;
         
-        if (s == null || s.length() < 1) return false;
-        
-        char ch1 = s.charAt(0);
-        if (ch1 == ')' || ch1 == '}' || ch1 == ']') return false;
-                
         Stack<Character> stack = new Stack<>();
         
-        for (Character ch : s.toCharArray()) {
-
-            if (ch == '(' || ch == '{' || ch == '[') {
-                stack.push(ch); // push opening paranthesis in the stack
-            
+        for (char ch : s.toCharArray()) {
+            if (ch == '{' || ch == '[' || ch == '(') {
+                stack.push(ch);
             } else {
-                if (!stack.empty()) {
-
-                    char top = stack.peek();
-
-        if ( (ch == ')' && top == '(' ) || (ch == '}' && top == '{' ) || (ch == ']' && top == '[' ) ) {
-                        stack.pop();
-                    }
-                    else {
-                        return false;
-                    }
-                } else return false; // if stack is already empty and you get closing paranthesis
+                if (stack.isEmpty())
+                    return false;
+                char curr = stack.pop();
+                
+                if ((ch == ')' && curr == '(') || (ch == '}' && curr == '{') || (ch == ']' && curr == '[') )
+                    continue;
+                else
+                    return false;
             }
-            
         }
-        
-        if (!stack.empty()) return false;
-        
-        return true;
+        return stack.isEmpty() ? true : false;
     }
 }
