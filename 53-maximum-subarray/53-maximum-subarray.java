a83226388720b40cd3982ddf79da4982e744e460
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-09-16 21:51:56 -0500

    Time: 4 ms (10.24%), Space: 74.2 MB (43.07%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,6 +1,7 @@
 class Solution {
-    // using Kadane's algorithm
-    public int maxSubArray(int[] nums) {
+    // 1st approach - using Kadane's algorithm
+    public int maxSubArray1(int[] nums) {
+        
         int max = Integer.MIN_VALUE;
         int currSum = 0;
         
@@ -16,4 +17,22 @@ class Solution {
         
         return max;
     }
+    
+    // 2nd approach
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        
+        int max = dp[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            
+            dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
+            
+            max = Math.max(max, dp[i]);
+        }
+        
+        return max;
+    }
+
 }
