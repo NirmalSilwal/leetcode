2f08b31c2c0315d0a22ab66d9bc675fe1b13e6d3
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-09-19 01:32:03 -0500

    Time: 1 ms (99.91%), Space: 53.6 MB (84.72%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -1,14 +1,11 @@
 class Solution {
+    // two pointer approach
     public void reverseString(char[] s) {
-        if (s.length < 1)
-            return;
-        
+       
         int low = 0, high = s.length - 1;
         
-        // two pointer approach
         while (low < high) {
-        
-            // swap low and high position character
+           
             char temp = s[low];
             s[low] = s[high];
             s[high] = temp;
