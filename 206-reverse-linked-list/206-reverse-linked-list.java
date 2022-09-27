b19e7bbd1438e9ac843dfca6040129a258cf139f
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-09-26 22:12:12 -0500

    Time: 0 ms (100.00%), Space: 43.1 MB (32.65%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -9,11 +9,8 @@
  * }
  */
 class Solution {
-    // iterative approach
-    /*
     public ListNode reverseList(ListNode head) {
-        if (head == null || head.next == null)
-            return head;
+        if (head == null || head.next == null) return head;
         
         ListNode prev = null, curr = head, nxt = head.next;
         
@@ -21,26 +18,13 @@ class Solution {
             curr.next = prev;
             prev = curr;
             curr = nxt;
-            nxt = nxt.next;
+            
+            if (nxt != null)
+                nxt = nxt.next;
         }
-        
         curr.next = prev;
         head = curr;
+        
         return head;
-    } 
-    */
-    
-    // recursive solution
-    public ListNode reverseList(ListNode head) {
-      
-        if (head == null || head.next == null)
-            return head;
-        
-        ListNode remaining = reverseList(head.next);
-        
-        head.next.next = head;
-        head.next = null;
-        
-        return remaining;
     }
 }
