84023ed681bdcddda56f2cf730add8e428e1dcb5
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-10-21 13:23:35 -0500

    Time: 23 ms (37.91%), Space: 95.4 MB (76.66%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -2,7 +2,8 @@ class Solution {
     // using prefix sum with hashmap
     // longest consecutive array with sum = total - target 
     // target = left side + right side removed elements
-    public int minOperations(int[] nums, int x) {
+    // O(N) time, O(N) space
+    public int minOperations1(int[] nums, int x) {
         
         int targetSum = Arrays.stream(nums).sum() - x;
         
@@ -26,4 +27,36 @@ class Solution {
          
         return operations == -1 ? -1 : nums.length - operations;
     }
+    
+    // using sliding window approach - O(N) time O(1) space
+    public int minOperations(int[] nums, int x) {
+        
+        int target = Arrays.stream(nums).sum() - x;
+
+        if (target < 0) return -1;
+        if (target == 0) return nums.length;
+        
+        int operations = -1, windowSum = 0;
+        int start = 0, end = 0;
+        
+        while (end < nums.length) {
+
+            windowSum += nums[end];
+            
+            // slide window from left to right
+            while (windowSum > target) {
+                windowSum -= nums[start];
+                start++;
+            }
+            
+            if (windowSum == target) {
+                operations = Math.max(operations, end - start + 1); 
+            } 
+            
+            end++;
+        }
+        
+        return operations == -1 ? -1 : nums.length - operations;
+    }
+    
 }
