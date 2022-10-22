dd53e9d51a989ffbe6aa5c739d699bab4408314e
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-10-22 16:19:45 -0500

    Time: 222 ms (5.81%), Space: 42.5 MB (6.88%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,27 +1,26 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         Set<List<Integer>> set = new HashSet<>();
-        helper(set, k, n, new HashSet<Integer>());
+        combinations(set, k, n, new HashSet<Integer>());
         return new ArrayList<>(set);
     }
     
-    private void helper(Set<List<Integer>> set, int k, int reqsum, Set<Integer> curr) {
-        if (k == 0) {
-            if (reqsum == 0) {
+    private void combinations(Set<List<Integer>> set, int totalElements, int totalSum, Set<Integer> curr) {
+        
+        if (totalElements == 0) {
+            if (totalSum == 0) {
                 set.add(new ArrayList<>(curr));
                 return;
-            } else {
-                return;
-            }
+            } else return;
         }
         
-        if (reqsum < 0) return;
+        if (totalSum < 0) return;
         
-        for (int num = 1; num <= 9; num++) {
-            if (!curr.contains(num)) {
-                curr.add(num);
-                helper(set, k - 1, reqsum - num, curr);
-                curr.remove(num);
+        for (int value = 1; value <= 9; value++) {
+            if (!curr.contains(value)) {
+                curr.add(value);
+                combinations(set, totalElements - 1, totalSum - value, curr);
+                curr.remove(value); // backtrack
             }
         }
     }
