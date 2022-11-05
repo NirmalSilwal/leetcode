8ebc292fa52f8ed560dd8c9e10d660b3b2dd9972
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-11-05 02:24:44 -0500

    Time: 1 ms (82.31%), Space: 42.7 MB (34.02%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,9 +1,40 @@
 class Solution {
     // brute force -O(n) time - linear search
-    public int search(int[] nums, int target) {
+    public int search1(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == target) return i;
         }
+        return -1;
+    }
+    
+    // optimal - binary search, O(logN) time
+    public int search(int[] nums, int target) {
+        
+        int low = 0, high = nums.length - 1;
+        
+        while (low <= high) {
+            
+            int mid = low + (high - low) / 2;
+            
+            if (nums[mid] == target) return mid;
+            
+            // check if left half is sorted or not
+            if (nums[mid] >= nums[low]) {
+                if (target >= nums[low] && target <= nums[mid]) {
+                    // target must be in this half
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (target >= nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        
         return -1;
     }
 }
