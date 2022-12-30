3ee413a128dc36c0407069353d2043cb46427a4f
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2022-12-30 13:59:16 -0600

    Time: 126 ms (89.29%), Space: 75.5 MB (91.72%) - LeetHub

---
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.java
@@ -0,0 +1,54 @@
+class Solution {    
+    public int[] getOrder(int[][] tasks) {
+        int n = tasks.length;
+        
+        Task[] taskWithIndex = new Task[n];
+        
+        for (int i = 0; i < n; i++) {
+            taskWithIndex[i] = new Task(i, tasks[i][0], tasks[i][1]);
+        }
+        // Arrays.sort(taskWithIndex, (a, b) -> a[1] - b[1]);
+        Arrays.sort(taskWithIndex, (a, b) -> {
+            return Integer.compare(a.enqueueTime, b.enqueueTime);
+        });
+        
+        PriorityQueue<Task> minHeap = new PriorityQueue<>((a, b) -> {
+            if (a.processingTime == b.processingTime) 
+                return Integer.compare(a.index, b.index);
+            return Integer.compare(a.processingTime, b.processingTime);
+        });
+        
+        int[] ans = new int[n];
+        int ansIndex = 0;
+        int taskIndex = 0; // task being processed
+        int currentTime = 0;
+        
+        while (ansIndex < n) {
+            
+            while (taskIndex < n && taskWithIndex[taskIndex].enqueueTime <= currentTime) {
+                minHeap.add(taskWithIndex[taskIndex++]);
+            }
+            
+            if (minHeap.isEmpty()) {
+                currentTime = taskWithIndex[taskIndex].enqueueTime;
+            } else {
+                currentTime += minHeap.peek().processingTime;
+                ans[ansIndex++] = minHeap.poll().index;
+            }
+        }
+        
+        return ans;
+    }
+    
+    class Task {
+        int index;
+        int enqueueTime;
+        int processingTime;
+
+        public Task(int idx, int enq, int proc) {
+            this.index = idx;
+            this.enqueueTime = enq;
+            this.processingTime = proc;
+        }
+    }
+}
