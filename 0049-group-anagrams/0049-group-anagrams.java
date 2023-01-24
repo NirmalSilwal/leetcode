fe52a5a79e091b784f6a894fc079d7ab8a97246e
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2023-01-23 20:44:36 -0600

    Time: 11 ms (57.17%), Space: 56 MB (23.80%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -3,32 +3,21 @@ class Solution {
         
         List<List<String>> result = new ArrayList<>();
         Map<String, List<String>> map = new HashMap();
-        
+
         for (String curr : strs) {
-            
-            // storing count of each characters of current string
-            int[] currCharCounts = new int[26];
-            for (char ch : curr.toCharArray()) {
-                currCharCounts[ch - 'a']++;
+
+            char[] chars = curr.toCharArray();
+            Arrays.sort(chars);
+            String sortedCurr = new String(chars);
+
+            if (!map.containsKey(sortedCurr)) {
+                List<String> currGroup = new ArrayList<>();
+                map.put(sortedCurr, currGroup);
             }
-            
-            StringBuffer sb = new StringBuffer();
-            for (int n : currCharCounts) {
-                sb.append(n);
-                sb.append("#"); // to make unique key for hashmap based on characters counts
-            }
-            
-            String currentKey = sb.toString();
-            
-            if (!map.containsKey(currentKey)) {
-                map.put(currentKey, new ArrayList<>());
-            }
-            
-            map.get(currentKey).add(curr);
+            map.get(sortedCurr).add(curr);
         }
-        
         result.addAll(map.values());
-        
+
         return result;
     }
 }
