76522d3186eb814d3c7a219c8dccae752422856b
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2023-01-23 20:58:01 -0600

    Time: 22 ms (25.71%), Space: 46.3 MB (50.58%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
+    public List<List<String>> groupAnagrams1(String[] strs) {
         
         List<List<String>> result = new ArrayList<>();
         Map<String, List<String>> map = new HashMap();
@@ -15,6 +15,37 @@ class Solution {
                 map.put(sortedCurr, currGroup);
             }
             map.get(sortedCurr).add(curr);
+        }
+        result.addAll(map.values());
+
+        return result;
+    }
+    
+    // 2nd approach
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        List<List<String>> result = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap();
+
+        for (String curr : strs) {
+
+            int[] charcounts = new int[26];
+            for (char ch : curr.toCharArray()) {
+                charcounts[ch - 'a']++;
+            }
+            
+            StringBuilder sb = new StringBuilder();
+            for (int count : charcounts) {
+                sb.append(count);
+                sb.append("#");
+            }
+            
+            String newkey = sb.toString();
+
+            if (!map.containsKey(newkey)) {
+                map.put(newkey, new ArrayList<>());
+            }
+            map.get(newkey).add(curr);
         }
         result.addAll(map.values());
 
