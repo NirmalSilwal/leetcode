a706a63388f6b3c3441892863b768d913c3684dc
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2023-02-08 17:50:18 -0600

    Time: 0 ms (100.00%), Space: 41 MB (40.31%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        
+        double[] ans = new double[2];
+
+        ans[0] = celsius + 273.15; // kelvin
+        ans[1] = celsius * 1.80 + 32.00; // farenheight
+
+        return ans;
+    }
+}
