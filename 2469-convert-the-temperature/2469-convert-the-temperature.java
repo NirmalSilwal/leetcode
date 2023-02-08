64c6fa6f582d03e9165d68c7f9d0dfb2142a0452
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2023-02-08 17:53:52 -0600

    Time: 0 ms (100.00%), Space: 40.2 MB (99.18%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.java
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -1,5 +1,5 @@
 class Solution {
-    public double[] convertTemperature(double celsius) {
+    public double[] convertTemperature1(double celsius) {
         
         double[] ans = new double[2];
 
@@ -8,4 +8,10 @@ class Solution {
 
         return ans;
     }
+    
+    public double[] convertTemperature(double celsius) {
+        
+        return new double[]{celsius + 273.15, celsius * 1.80 + 32.00};   
+    }
+    
 }
