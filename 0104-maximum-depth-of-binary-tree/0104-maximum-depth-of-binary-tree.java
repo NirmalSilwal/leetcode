4f0744770162372466c19afa6f50474178a5d246
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2023-02-16 17:42:35 -0600

    Time: 3 ms (5.09%), Space: 42.3 MB (42.10%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -14,7 +14,7 @@
  * }
  */
 class Solution {
-    public int maxDepth(TreeNode root) {
+    public int maxDepth1(TreeNode root) {
         
         if (root == null) return 0;
         
@@ -25,4 +25,38 @@ class Solution {
         
         return Math.max(leftDepth, rightDepth);
     }
+    
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int depth = 1;
+        
+        Stack<TreeNode> nodeStack = new Stack<>();
+        Stack<Integer> depthStack = new Stack();
+        nodeStack.push(root);
+        depthStack.push(1);
+        
+        while (!nodeStack.isEmpty()) {
+            
+            TreeNode currentNode = nodeStack.pop();
+            int currentDepth = depthStack.pop();
+            
+            if (currentNode.left == null && currentNode.right == null) {
+                depth = Math.max(depth, currentDepth);
+            }
+            
+            if (currentNode.left != null) {
+                nodeStack.push(currentNode.left);
+                depthStack.push(currentDepth + 1);
+            }
+            
+            if (currentNode.right != null) {
+                nodeStack.push(currentNode.right);
+                depthStack.push(currentDepth + 1);
+            }
+            
+        }
+        
+        return depth;
+    }
+    
 }
