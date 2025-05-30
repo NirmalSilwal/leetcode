4d9e554c449b87d9b819e58485bbbb2a19325b0b
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2025-05-30 00:47:46 -0500

    Time: 3 ms (84.27%) | Memory: 50 MB (34.27%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,13 +1,13 @@
 class Solution {
+    // O(1) space solution
     public int numIslands(char[][] grid) {
         
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
         int count = 0;
 
         for (int row = 0; row < grid.length; row++) {
             for (int col = 0; col < grid[row].length; col++) {
-                if (!visited[row][col] && grid[row][col] == '1') {
-                    dfs (row, col, visited, grid);
+                if (grid[row][col] == '1') {
+                    dfs (row, col, grid);
                     count++;
                 }
             }
@@ -15,17 +15,16 @@ class Solution {
         return count;
     }
 
-    private void dfs (int row, int col, boolean[][] visited, char[][] grid) {
+    private void dfs (int row, int col, char[][] grid) {
         
-        // boundary checks
-        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || visited[row][col] == true || grid[row][col] == '0')
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == '0')
             return;
 
-        visited[row][col] = true;
+        grid[row][col] = '0'; // mark that cell as visited
 
-        dfs (row, col - 1, visited, grid); // left
-        dfs (row, col + 1, visited, grid); // right
-        dfs (row - 1, col, visited, grid); // up
-        dfs (row + 1, col, visited, grid); // down
+        dfs (row, col - 1, grid); // left
+        dfs (row, col + 1, grid); // right
+        dfs (row - 1, col, grid); // up
+        dfs (row + 1, col, grid); // down
     }
 }
