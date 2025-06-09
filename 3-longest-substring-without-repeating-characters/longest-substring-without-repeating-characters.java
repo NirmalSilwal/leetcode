a0dd39663a256958483449992b9bc07840fe789a
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2025-06-09 00:28:53 -0500

    Time: 3 ms (91.42%) | Memory: 43.9 MB (86.68%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
+    // brute force approach
+    public int lengthOfLongestSubstringBruteForce(String s) {
         int maxLen = 0;
 
         for (int i = 0; i < s.length(); i++) {
@@ -16,6 +17,31 @@ class Solution {
 
             maxLen = Math.max(maxLen, charSet.size());
         }
+        return maxLen;
+    }
+
+    // sliding window and two pointer approach
+    public int lengthOfLongestSubstring(String s) {
+        int[] hash = new int[256];
+        Arrays.fill(hash, -1);
+
+        int left = 0, right = 0, maxLen = 0, currLen = 0;
+
+        while (right < s.length()) {
+
+            if (hash[s.charAt(right)] != -1) {
+                // means that character is already present in our hashmap
+                if (hash[s.charAt(right)] >= left) {
+                    left = hash[s.charAt(right)] + 1;
+                }
+            }
+            currLen = right - left + 1;
+            maxLen = Math.max(maxLen, currLen);
+
+            hash[s.charAt(right)] = right;
+            right++;
+        }
+
 
         return maxLen;
     }
