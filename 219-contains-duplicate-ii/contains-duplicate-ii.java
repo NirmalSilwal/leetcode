0ef31a47d22d92bb247c8f49bb7c30fd664ec82d
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2025-06-22 18:19:51 -0500

    Time: 1177 ms (6.97%) | Memory: 58.2 MB (93.60%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,5 +1,6 @@
 class Solution {
-    public boolean containsNearbyDuplicate(int[] nums, int k) {
+    // O(n) time, O(k) space
+    public boolean containsNearbyDuplicateOptimal(int[] nums, int k) {
         Set<Integer> window = new HashSet<>();
 
         for (int i = 0; i < nums.length; i++) {
@@ -13,4 +14,17 @@ class Solution {
         }
         return false;
     }
+
+    // O(n*k) time, O(1) space
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            // sliding window range at each step, i to i+k
+            for (int j = i + 1; j < n && j <= i + k; j++) {
+                if (nums[i] == nums[j]) return true;
+            }
+        }
+        return false;
+    }
+
 }
