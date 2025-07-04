9288787597693c7a31920d3d625fbea0922bd8d3
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2025-07-04 17:43:18 -0500

    Time: 6 ms (72.10%) | Memory: 45 MB (23.98%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,48 +1,20 @@
 class Solution {
-    // brute force approach
-    public int lengthOfLongestSubstringBruteForce(String s) {
-        int maxLen = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            Set<Character> charSet = new HashSet<>();
-            charSet.add(s.charAt(i));
-
-            for (int j = i + 1; j < s.length(); j++) {
-                if (!charSet.contains(s.charAt(j))) {
-                    charSet.add(s.charAt(j));
-                } else {
-                    break;
-                }
-            }
-
-            maxLen = Math.max(maxLen, charSet.size());
-        }
-        return maxLen;
-    }
-
-    // sliding window and two pointer approach
     public int lengthOfLongestSubstring(String s) {
-        int[] hash = new int[256];
-        Arrays.fill(hash, -1);
+        Set<Character> seen = new HashSet<>();
+        int maxLen = 0, start = 0;
 
-        int left = 0, right = 0, maxLen = 0, currLen = 0;
-
-        while (right < s.length()) {
-
-            if (hash[s.charAt(right)] != -1) {
-                // means that character is already present in our hashmap
-                if (hash[s.charAt(right)] >= left) {
-                    left = hash[s.charAt(right)] + 1;
-                }
+        for(int end = 0; end < s.length(); end++) {
+            char currChar = s.charAt(end);
+            
+            // sliding window to remove duplicates, shink window from beginning
+            while (seen.contains(currChar)) {
+                seen.remove(s.charAt(start));
+                start++;
             }
-            currLen = right - left + 1;
-            maxLen = Math.max(maxLen, currLen);
-
-            hash[s.charAt(right)] = right;
-            right++;
+            seen.add(currChar);
+            maxLen = Math.max(maxLen, end - start + 1);
         }
-
-
+        
         return maxLen;
     }
 }
