0b863c48479eb1fcb11ca8afe60d598c5bb8bb67
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2025-07-25 00:27:38 -0500

    Time: 31 ms (56.67%) | Memory: 51.8 MB (41.95%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -4,32 +4,30 @@ class Solution {
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates
+            if (i > 0 && nums[i] == nums[i-1]) continue; 
 
             int left = i + 1, right = nums.length - 1;
 
             while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
+                int threeSum = nums[i] + nums[left] + nums[right];
+                if (threeSum == 0) {
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    // skip duplicate second and third element
+                    
                     while (left < right && nums[left] == nums[left + 1]) left++;
 
                     while (left < right && nums[right] == nums[right - 1]) right--;
 
                     left++;
                     right--;
-                } else if (sum < 0) {
-                    // since sum is less than 0, we need larger number in sorted list
+                } else if (threeSum < 0) {
                     left++;
                 } else {
-                    // sum is greater so need smaller one
                     right--;
                 }
+               
             }
         }
+
         return result;
     }
 }
